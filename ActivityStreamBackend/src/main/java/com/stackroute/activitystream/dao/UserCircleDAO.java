9593e419262a389a45ba69ab1bb6e252d9eea26b
abbas886 <abbas886@yahoo.com>
2017-10-03 16:39:08 +0530

    Need more methods

--- ActivityStreamBackend/src/main/java/com/stackroute/activitystream/dao/UserCircleDAO.java
+++ ActivityStreamBackend/src/main/java/com/stackroute/activitystream/dao/UserCircleDAO.java
@@ -4,5 +4,10 @@ public interface UserCircleDAO
 {
 	public boolean addUserToCircle(int circleId,String emailId);
 	public boolean removeUserFromCircle(int circleId,String emailId);
+	
+	//Need to add other methods  like
+	// get My circles
+	// get All Circles
+	// get All Users related to particular circle etc.,
 
 }
