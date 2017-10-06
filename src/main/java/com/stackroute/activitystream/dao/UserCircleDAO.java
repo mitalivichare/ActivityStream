958d4b2c6925f5961bd2b416d7b95ab89ee40bdc
abbas886 <abbas886@yahoo.com>
2017-10-06 16:46:57 +0530

    need extra method

--- src/main/java/com/stackroute/activitystream/dao/UserCircleDAO.java
+++ src/main/java/com/stackroute/activitystream/dao/UserCircleDAO.java
@@ -5,7 +5,13 @@ public interface UserCircleDAO
 	public boolean addUserToCircle(int circleId,String emailId);
 	public boolean removeUserFromCircle(int circleId,String emailId);
 	
+	
 	//get All circle method present in CircleDAO
 	//get my circle method present in UserCircleDAOImpl as a private method
+	
+	//get my circles method should not be private.  It should be there in this interface also, so that it can be acces from outside.
+	//i.e., from micro service.
+	
+	
 
 }
